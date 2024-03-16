a7a76154293a429e7c1c2a10bc4f78f6ad1d6a2e
sideshowbobgot <sideshowbobgot@gmail.com>
2024-03-16 21:50:25 +0200

    added conan lib

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+import os.path
+
+from conan import ConanFile
+from conan.tools.cmake import CMake
+
+
+class LinalgRecipe(ConanFile):
+    name = "imu"
+    version = "1.0.0"
+    generators = ("CMakeToolchain", "CMakeDeps")
+    settings = ("os", "build_type", "arch", "compiler")
+    exports_sources = ("CMakeLists.txt", "include/*")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.18]")
+
+    def layout(self):
+        self.folders.build = "conan_external"
+        self.folders.generators = os.path.join(self.folders.build, "generators")
